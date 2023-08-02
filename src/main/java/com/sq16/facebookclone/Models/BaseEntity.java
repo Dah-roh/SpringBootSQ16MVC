7c4a678d923305bedeaddb24266be5716173859d
daro-decagon <gadibiao@decagonhq.com>
2023-08-02 13:58:24 +0100

    adds post and comment implementation

--- src/main/java/com/sq16/facebookclone/Models/BaseEntity.java
+++ src/main/java/com/sq16/facebookclone/Models/BaseEntity.java
@@ -1,14 +1,20 @@
 package com.sq16.facebookclone.Models;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
-public abstract class BaseEntity {
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity implements Serializable {
     @CreationTimestamp
     @Column(name = "created_at")
     private Date createdAt;
